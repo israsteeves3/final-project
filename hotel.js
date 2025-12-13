240ae32e9c4f030bc191028ea9afb1d262042c2e
israsteeves3 <isra.steeves@edu.sait.ca>
2025-12-12 21:04:32 -0700

    more.

--- hotel.js
+++ hotel.js
@@ -14,11 +14,11 @@ fetch("http://api.openweathermap.org/data/2.5/weather?q=Cancun&appid=6cff9a6c350
 .then(response => response.json())
 .then(data => {
     console.log(data);
-    temperature.textContent = "Temperature: "+data.main.temp+" \u00b0C";
-    description.textContent = "Description "+data.weather[0].description; //0 gives current weather, 1 is tomorrow, think of it like an array
-    high.textContent = "High " +data.main.temp_max+" \u00b0C"; //its like file folders and pathing to target the right thing
-    low.textContent = "Low " +data.main.temp_min+" \u00b0C";
-    feelslike.textContent = "Feels like " +data.main.feels_like+" \u00b0C";
+    temperature.textContent = "Current temperature: "+data.main.temp+" \u00b0C";
+    description.textContent = "Current weather: "+data.weather[0].description; //0 gives current weather, 1 is tomorrow, think of it like an array
+    high.textContent = "High today is " +data.main.temp_max+" \u00b0C"; //its like file folders and pathing to target the right thing
+    low.textContent = "Low today is " +data.main.temp_min+" \u00b0C";
+    feelslike.textContent = "It currently feels like " +data.main.feels_like+" \u00b0C";
 
 //get the icon for the weather 04d is within the API it directly correlates to the icon needed
 let iconCode = data.weather[0].icon;
